d15980330b832e1075ae97ccdd042a907833d2d2
BT_1N3_12@BKTECH.LOCAL <BT_1N3_12@BKTECH.LOCAL>
2018-12-12 11:41:21 -0500

    Initial commit

---
+++ src/Book.java
@@ -0,0 +1,38 @@
+public class Book {
+    private String name, author;
+    private int price;
+
+    public Book(String name, String author, int price) {
+        this.name = name;
+        this.author = author;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String toString() {
+        return name + " by " + author + ". Costs " + price;
+    }
+}
---
+++ src/CSVReaderInJava.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CSVReaderInJava {
+    public static void main(String[] arg) {
+        List<Book> books = readBooksFromCSV("books.txt");
+
+        for (Book b : books)
+            System.out.println(b);
+    }
+
+    private static List<Book> readBooksFromCSV(String fileName) {
+        List<Book> books = new ArrayList<>();
+        Path pathToFile = Paths.get(fileName);
+        System.out.println(pathToFile);
+
+        try (BufferedReader br = Files.newBufferedReader(pathToFile, StandardCharsets.US_ASCII)) {
+            String line = br.readLine();
+
+            // The line is null if the EOF (End of file) is reached.
+            while (line != null) {
+                // The comma (',') is the delimiter.
+                String[] attributes = line.split(",");
+                books.add(new Book(attributes[0], attributes[2], Integer.parseInt(attributes[1])));
+                line = br.readLine();
+            }
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+        return books;
+    }
+}
